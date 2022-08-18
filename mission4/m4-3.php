968628ffbd0f5456f57a155c2a48b7dca29238b1
IshikawaTaichi0805 <111512355+IshikawaTaichi0805@users.noreply.github.com>
2022-08-18 22:34:07 +0900

    Update m4-3.php

--- mission4/m4-3.php
+++ mission4/m4-3.php
@@ -1,7 +1,7 @@
 <?php
-    $dsn = 'mysql:dbname=tb240115db;host=localhost';
-    $user = 'tb-240115';
-    $password = 'hGMhzFK95E';
+   $dsn = 'データベース';//繋げたいデータベースを書いてる
+    $user = 'ID';
+    $password = 'パスワード';
     $pdo = new PDO($dsn, $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
     $sql ='SHOW TABLES';
     $result = $pdo -> query($sql);
