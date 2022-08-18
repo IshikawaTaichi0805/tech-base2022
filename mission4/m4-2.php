b9c84ed170311881a69097bb127f124b8d2058a1
IshikawaTaichi0805 <111512355+IshikawaTaichi0805@users.noreply.github.com>
2022-08-18 22:33:51 +0900

    Update m4-2.php

--- mission4/m4-2.php
+++ mission4/m4-2.php
@@ -1,9 +1,9 @@
 
 
     <?php
-    $dsn = 'mysql:dbname=tb240115db;host=localhost';
-    $user = 'tb-240115';
-    $password = 'hGMhzFK95E';
+    $dsn = 'データベース';//繋げたいデータベースを書いてる
+    $user = 'ID';
+    $password = 'パスワード';
     $pdo = new PDO($dsn, $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
     $sql = "CREATE TABLE IF NOT EXISTS tbtest"
     ." ("
